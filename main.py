b9cad0d6a49cc076ee24018b6674179d30813fae
Jinesh Mehta <mehtajineshs@gmail.com>
2022-04-10 17:32:22 -0400

    completed classification using pre-trained bert

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Main file
 """
 import re
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,12 +9,22 @@ from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import silhouette_score
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from nltk import sent_tokenize, word_tokenize
+import torch
+from tqdm.notebook import tqdm
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification
+from transformers import AdamW, get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 
 stopwords = stopwords.words('english')
 stemmer = SnowballStemmer("english")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def read_data(csv_file):
@@ -87,7 +98,8 @@ def clustering_errors(k, data):
     kmeans = KMeans(n_clusters=k).fit(data)
     predictions = kmeans.predict(data)
     #cluster_centers = kmeans.cluster_centers_
-    # errors = [mean_squared_error(row, cluster_centers[cluster]) for row, cluster in zip(data.values, predictions)]
+    # errors = [mean_squared_error(row, cluster_centers[cluster])
+    # for row, cluster in zip(data.values, predictions)]
     # return sum(errors)
     silhouette_avg = silhouette_score(data, predictions)
     return silhouette_avg
@@ -132,6 +144,83 @@ def perform_k_means_mini_batch(td_idf_transformed_data, vector, n_clusters, movi
     return data_frame_cluster_predictions
 
 
+def f1_score_func(preds, labels):
+    """_summary_
+
+    Args:
+        preds (_type_): _description_
+        labels (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return f1_score(labels_flat, preds_flat, average='weighted')
+
+
+def accuracy_per_class(preds, labels):
+    """_summary_
+
+    Args:
+        preds (_type_): _description_
+        labels (_type_): _description_
+    """
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    for label in np.unique(labels_flat):
+        y_preds = preds_flat[labels_flat == label]
+        y_true = labels_flat[labels_flat == label]
+        print(f'Class: {label}')
+        print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
+
+
+def evaluate(model, dataloader_val):
+    """_summary_
+
+    Args:
+        model (_type_): _description_
+        dataloader_val (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    model.eval()
+
+    loss_val_total = 0
+    predictions, true_vals = [], []
+
+    for batch in dataloader_val:
+
+        batch = tuple(b.to(device) for b in batch)
+
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                  }
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        loss = outputs[0]
+        logits = outputs[1]
+        loss_val_total += loss.item()
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = inputs['labels'].cpu().numpy()
+        predictions.append(logits)
+        true_vals.append(label_ids)
+
+    loss_val_avg = loss_val_total/len(dataloader_val)
+
+    predictions = np.concatenate(predictions, axis=0)
+    true_vals = np.concatenate(true_vals, axis=0)
+
+    return loss_val_avg, predictions, true_vals
+
+
 def main(csv_file):
     """main function
     """
@@ -152,11 +241,164 @@ def main(csv_file):
         movie_data_frame["description"])
     #ideal_n_cluster = identify_best_cluster_value(tf_idf_transformed_data)
     # identified this value of 37 using elbow method
-    ideal_n_cluster = 37
+    ideal_n_cluster = 10
     clustered_labels = perform_k_means_mini_batch(
         tf_idf_transformed_data, vector, ideal_n_cluster, movie_data_frame["description"])
     movie_data_frame_labelled = movie_data_frame.assign(
         cluster=clustered_labels)
+    # print(movie_data_frame_labelled['cluster'].value_counts())
+    # print(movie_data_frame_labelled.cluster.values)
+    # print(movie_data_frame_labelled.cluster.values.shape)
+    # print(movie_data_frame_labelled.description.values)
+    # print(movie_data_frame_labelled.description.values.shape)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        movie_data_frame_labelled.description.values,
+        movie_data_frame_labelled.cluster.values,
+        test_size=0.15,
+        random_state=42, stratify=movie_data_frame_labelled.cluster.values)
+
+    movie_data_frame_labelled['data_type'] = [
+        'not_set']*movie_data_frame_labelled.shape[0]
+
+    for item in movie_data_frame_labelled.description:
+        if item in X_train:
+            movie_data_frame_labelled.loc[movie_data_frame_labelled.description ==
+                                          item, 'data_type'] = 'train'
+        else:
+            movie_data_frame_labelled.loc[movie_data_frame_labelled.description ==
+                                          item, 'data_type'] = 'val'
+
+    # print(movie_data_frame_labelled.groupby(
+    #     ['cluster', 'data_type']).count())
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
+                                              do_lower_case=True)
+
+    encoded_data_train = tokenizer.batch_encode_plus(
+        movie_data_frame_labelled[movie_data_frame_labelled.data_type ==
+                                  'train'].description.values,
+        add_special_tokens=True,
+        return_attention_mask=True,
+        pad_to_max_length=True,
+        max_length=256,
+        return_tensors='pt'
+    )
+
+    encoded_data_val = tokenizer.batch_encode_plus(
+        movie_data_frame_labelled[movie_data_frame_labelled.data_type ==
+                                  'val'].description.values,
+        add_special_tokens=True,
+        return_attention_mask=True,
+        pad_to_max_length=True,
+        max_length=256,
+        return_tensors='pt'
+    )
+
+    input_ids_train = encoded_data_train['input_ids']
+    attention_masks_train = encoded_data_train['attention_mask']
+    labels_train = torch.tensor(
+        movie_data_frame_labelled[movie_data_frame_labelled.data_type ==
+                                  'train'].cluster.values).type(torch.LongTensor)
+
+    input_ids_val = encoded_data_val['input_ids']
+    attention_masks_val = encoded_data_val['attention_mask']
+    labels_val = torch.tensor(
+        movie_data_frame_labelled[movie_data_frame_labelled.data_type == 'val'].cluster.values).type(torch.LongTensor)
+
+    dataset_train = TensorDataset(
+        input_ids_train, attention_masks_train, labels_train)
+    dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
+
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased",
+        num_labels=len(
+            movie_data_frame_labelled['cluster'].unique()),
+        output_attentions=False,
+        output_hidden_states=False)
+    batch_size = 3
+
+    dataloader_train = DataLoader(dataset_train,
+                                  sampler=RandomSampler(dataset_train),
+                                  batch_size=batch_size)
+
+    dataloader_validation = DataLoader(dataset_val,
+                                       sampler=SequentialSampler(dataset_val),
+                                       batch_size=batch_size)
+    optimizer = AdamW(model.parameters(),
+                      lr=1e-5,
+                      eps=1e-8)
+
+    epochs = 5
+
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0,
+                                                num_training_steps=len(dataloader_train)*epochs)
+    seed_val = 17
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
+
+    for epoch in tqdm(range(1, epochs+1)):
+
+        model.train()
+
+        loss_train_total = 0
+
+        progress_bar = tqdm(dataloader_train, desc='Epoch {:1d}'.format(
+            epoch), leave=False, disable=False)
+        for batch in progress_bar:
+
+            model.zero_grad()
+
+            batch = tuple(b.to(device) for b in batch)
+
+            inputs = {'input_ids':      batch[0],
+                      'attention_mask': batch[1],
+                      'labels':         batch[2],
+                      }
+
+            outputs = model(**inputs)
+
+            loss = outputs[0]
+            loss_train_total += loss.item()
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            optimizer.step()
+            scheduler.step()
+
+            progress_bar.set_postfix(
+                {'training_loss': '{:.3f}'.format(loss.item()/len(batch))})
+
+        torch.save(model.state_dict(),
+                   f'data_volume/finetuned_BERT_epoch_{epoch}.model')
+
+        tqdm.write(f'\nEpoch {epoch}')
+
+        loss_train_avg = loss_train_total/len(dataloader_train)
+        tqdm.write(f'Training loss: {loss_train_avg}')
+
+        val_loss, predictions, true_vals = evaluate(
+            model, dataloader_validation)
+        val_f1 = f1_score_func(predictions, true_vals)
+        tqdm.write(f'Validation loss: {val_loss}')
+        tqdm.write(f'F1 Score (Weighted): {val_f1}')
+
+        model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_labels=len(
+                movie_data_frame_labelled['cluster'].unique()),
+            output_attentions=False,
+            output_hidden_states=False)
+
+    model.to(device)
+    model.load_state_dict(torch.load(
+        'data_volume/finetuned_BERT_epoch_1.model', map_location=torch.device('cpu')))
+
+    _, predictions, true_vals = evaluate(model, dataloader_validation)
+    accuracy_per_class(predictions, true_vals)
 
 
 if __name__ == '__main__':
